ae130b982a51037978df347fe29556fbd4842703
Tal <taldanielm@campus.technion.ac.il>
2017-10-01 16:44:59 +0300

    v0.2

--- app/src/main/java/com/example/android/stratumminer/MainActivity.java
+++ app/src/main/java/com/example/android/stratumminer/MainActivity.java
@@ -242,6 +242,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (android.os.Build.VERSION.SDK_INT > 9)
+        {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
+
         Log.i("LC", "Main: in onCreate()");
 //        setTitle("StratumMiner");
 
